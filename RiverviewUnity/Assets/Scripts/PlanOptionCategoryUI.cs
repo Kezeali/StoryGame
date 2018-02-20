e14c36b2e263a34dc8e15beb6982322eb032f621
Ellie Hayward <kezeali@gmail.com>
2018-02-20 09:29:18 +0000

    Weekly planner UI greybox impl. functional

---
+++ RiverviewUnity/Assets/Scripts/PlanOptionCategoryUI.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using NotABear;
+
+public class PlanOptionCategoryUI : MonoBehaviour
+{
+	[SerializeField]
+	public Transform optionsContainer;
+	[SerializeField]
+	public Button toggle;
+	[SerializeField]
+	public Text title;
+
+	public Animator animator;
+
+	[System.NonSerialized]
+	public SubjectData subject;
+	[System.NonSerialized]
+	public List<PlanOptionUI> optionUis = new List<PlanOptionUI>();
+
+	public void Awake()
+	{
+		Debug.Assert(this.toggle != null);
+
+		this.toggle.onClick.AddListener(Toggle);
+	}
+
+	public void Toggle()
+	{
+		animator.SetBool("open", !animator.GetBool("open"));
+	}
+
+	public void Clear()
+	{
+		for (int i = 0; i < this.optionUis.Count; ++i)
+		{
+			Object.Destroy(this.optionUis[i].gameObject);
+		}
+		this.optionUis.Clear();
+		this.subject = null;
+	}
+}
