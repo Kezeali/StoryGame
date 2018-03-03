fff09077405b0db4a9bbe4a90391239df00466f3
Ellie Hayward <kezeali@gmail.com>
2018-03-03 05:25:15 +0000

    Menu naviagation.

--- RiverviewUnity/Assets/Scripts/SceneField.cs
+++ RiverviewUnity/Assets/Scripts/SceneField.cs
@@ -1,19 +1,27 @@
 using UnityEngine;
 
 [System.Serializable]
-public class SceneField
+public struct SceneField
 {
 	[SerializeField]
 	private Object sceneAsset;
+	
 	[SerializeField]
-	private string scenePath = "";
+	private string scenePath;
+
 	public string ScenePath
 	{
 		get { return scenePath; }
 	}
+
 	// makes it work with the existing Unity methods (LoadLevel/LoadScene)
 	public static implicit operator string(SceneField sceneField)
 	{
 		return sceneField.scenePath;
+	}
+
+	public override string ToString()
+	{
+		return this.scenePath;
 	}
 }
