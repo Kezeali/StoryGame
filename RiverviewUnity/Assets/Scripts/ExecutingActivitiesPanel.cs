ff671ab148c4c96d5a50e799dc933802f4e2aa46
elliot <elliot@535f3ce7-6fc1-4d6c-a841-952e6e1501c6>
2018-04-12 11:16:58 +0000

    WIP: executing activities panel.

---
+++ RiverviewUnity/Assets/Scripts/ExecutingActivitiesPanel.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using Cloverview;
+using System.Collections.Generic;
+using YamlDotNet.Serialization;
+
+namespace Cloverview
+{
+
+public class ExecutingActivitiesPanel : MonoBehaviour, IDataUser<SaveData>, IDataUser<PlanExecutor>
+{
+	// TODO(elliot): make a list of executor ids to look for?
+	[SerializeField]
+	string executorId;
+
+	public void OnEnable()
+	{
+		App.Register<SaveData>(this);
+		App.instance.GetExecutor(executorId, this);
+	}
+
+	public void Initialise(SaveData saveData)
+	{
+	}
+
+	public void Initialise(PlanExecutor planExecutor)
+	{
+	}
+}
+
+}
